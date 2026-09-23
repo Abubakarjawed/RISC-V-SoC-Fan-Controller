6acacf7270697880f60c5c187f927c68e79019e1
Syeda Ayesha Ali <syayeshaali@gmail.com>
2026-09-24 00:52:28 +0500

    Change PADDR, PWDATA, and PRDATA to 32 bits

--- sram_soc_top.sv
+++ sram_soc_top.sv
@@ -6,9 +6,9 @@ module sram_soc_top (
     input  logic       PSEL,
     input  logic       PENABLE,
     input  logic       PWRITE,
-    input  logic [7:0] PADDR,
-    input  logic [7:0] PWDATA,
-    output logic [7:0] PRDATA,
+    input  logic [31:0] PADDR,
+    input  logic [31:0] PWDATA,
+    output logic [31:0] PRDATA,
     output logic       PREADY,
     output logic       PSLVERR
 );
